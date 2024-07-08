9893d709a26a7ef4d15647f7225915cac2796c63
travel15740 <travel15740@gmail.com>
2024-07-08 11:35:49 +0800

    建立有關店家新增、編輯、刪除之邏輯

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -3,6 +3,7 @@ const { Restaurant } = require('../models')
 
 // 載入所需工具
 const { Op, literal } = require('sequelize') // 引入 sequelize 查詢符、啟用 SQL 語法
+const { localCoverHandler } = require('../helpers/file-helpers') // 負責上傳餐廳封面照片
 
 const restaurantController = {
   getRestaurants: (req, res, next) => {
@@ -21,7 +22,7 @@ const restaurantController = {
     return Restaurant.findAndCountAll({
       raw: true,
       where: whereClause,
-      order: [['id', 'ASC']]
+      order: [['id', 'DESC']]
     })
       .then(restaurants => {
         const data = restaurants.rows
@@ -38,25 +39,100 @@ const restaurantController = {
     return Restaurant.findByPk(restaurantId, {
       raw: true
     })
-      .then(restaurant => res.render('restaurants/restaurant', { restaurant }))
+      .then(restaurant => {
+        if (!restaurant) throw new Error('店家不存在!')
+        return res.render('restaurants/restaurant', { restaurant })
+      })
       .catch(err => next(err))
   },
   newRestaurant: (req, res, next) => {
     return res.render('restaurants/new')
   },
   postRestaurant: (req, res, next) => {
-    res.send('功能開發中!')
+    // 拿到表單變數
+    const { name, category, location, googleMap, phone, rating, description } = req.body
+    const file = req.file
+
+    // 檢驗必填欄位是否存在
+    if (!name) throw new Error('店家名稱為必填欄位!')
+    // 把取出的檔案 file 傳給 file-helper 處理
+    return localCoverHandler(file)
+      .then(filePath => {
+        return Restaurant.create({
+          name,
+          category,
+          location,
+          googleMap,
+          phone,
+          rating,
+          description,
+          image: filePath || null
+        })
+      })
+      .then(newRestaurant => {
+        req.flash('success_messages', '成功新增店家資訊!')
+        res.redirect('/restaurants')
+      })
+      .catch(err => next(err))
   },
   editRestaurant: (req, res, next) => {
     const restaurantId = req.params.id
     return Restaurant.findByPk(restaurantId, {
       raw: true
     })
-      .then(restaurant => res.render('restaurants/edit', { restaurant }))
+      .then(restaurant => {
+        if (!restaurant) throw new Error('店家不存在!')
+        return res.render('restaurants/edit', { restaurant })
+      })
+
       .catch(err => next(err))
   },
   putRestaurant: (req, res, next) => {
-    res.send('功能開發中!')
+    const RestaurantId = req.params.id // 拿到路由變數
+
+    // 拿到表單變數
+    const { name, category, location, googleMap, phone, rating, description } = req.body
+    const file = req.file
+
+    // 檢驗必填欄位是否存在
+    if (!name) throw new Error('店家名稱為必填欄位!')
+
+    return Promise.all([
+      Restaurant.findByPk(RestaurantId),
+      localCoverHandler(file) // 把取出的檔案 file 傳給 file-helper 處理
+    ])
+
+      .then(([restaurant, filePath]) => {
+        if (!restaurant) throw new Error('店家不存在!')
+        return restaurant.update({
+          name,
+          category,
+          location,
+          googleMap,
+          phone,
+          rating,
+          description,
+          image: filePath || restaurant.image
+        })
+      })
+      .then(newRestaurant => {
+        req.flash('success_messages', '成功修改店家資訊!')
+        res.redirect(`/restaurants/${newRestaurant.id}`)
+      })
+      .catch(err => next(err))
+  },
+  deleteRestaurant: (req, res, next) => {
+    const RestaurantId = req.params.id // 拿到路由變數
+    return Restaurant.findByPk(RestaurantId)
+      .then(restaurant => {
+        if (!restaurant) throw new Error('店家不存在!')
+        return restaurant.destroy()
+      })
+      .then(deletedRestaurant => {
+        req.flash('success_messages', '成功刪除店家!')
+        return res.redirect('back')
+      })
+      .catch(err => next(err))
   }
 }
 
--- routes/pages/index.js
+++ routes/pages/index.js
@@ -9,6 +9,7 @@ const restaurantController = require('../../controllers/restaurant-controller')
 const passport = require('../../config/passport') // 負責使用者登入與驗證相關
 const { authenticated } = require('../../middlewares/auth')
 const { generalErrorHandler } = require('../../middlewares/error-handler')
+const upload = require('../../middlewares/multer') // 負責上傳圖片
 
 // 設計路由
 // 設計路由: 使用者登入相關
@@ -21,11 +22,11 @@ router.get('/logout', userController.logOut)
 // 設計路由: 餐廳相關
 router.get('/restaurants', authenticated, restaurantController.getRestaurants)
 router.get('/restaurants/new', authenticated, restaurantController.newRestaurant)
-router.post('/restaurants', authenticated, restaurantController.postRestaurant)
-
+router.post('/restaurants', authenticated, upload.single('image'), restaurantController.postRestaurant)
 router.get('/restaurants/:id', authenticated, restaurantController.getRestaurant)
 router.get('/restaurants/:id/edit', authenticated, restaurantController.editRestaurant)
-router.put('/restaurants/:id', authenticated, restaurantController.putRestaurant)
+router.put('/restaurants/:id', authenticated, upload.single('image'), restaurantController.putRestaurant)
+router.delete('/restaurants/:id', authenticated, restaurantController.deleteRestaurant)
 
 // 設計路由: 應用程式報錯
 router.get('/', (req, res) => res.redirect('/restaurants'))
